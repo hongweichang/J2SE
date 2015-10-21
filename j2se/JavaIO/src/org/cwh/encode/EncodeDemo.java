beec4366157f952b7bf0ace3203f30f8827e4d51
HouJh <houjunhu_2009@163.com>
2015-10-21 11:07:37 +0800

    测试提交

--- j2se/JavaIO/src/org/cwh/encode/EncodeDemo.java
+++ j2se/JavaIO/src/org/cwh/encode/EncodeDemo.java
@@ -6,9 +6,7 @@ public class EncodeDemo {
 	public static void main(String[] args) {
 		//得到系统配置信息
 		System.getProperties().list(System.out);
-		
-		
-		
+		System.out.println("TEST OUT.");
 	}
 
 }
